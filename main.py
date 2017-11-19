ee2a097bb23062fb0097526f60cbad99975ab790
Dmitry Sorokin <dmitrii.sorokin@phystech.edu>
2017-11-19 21:34:43 +0300

    support different types of annotations

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from pdf_highlighter import highlight, highlight_file
+from pdf_highlighter import create_highlight, create_circle, create_square, highlight_file
 
-highlights_dict = {
+annotations_dict = {
     0: [
-        highlight(50, 740, 560, 800, color=(1, 0, 1)),
-        highlight(50, 650, 500, 680, color=(1, 0, 0))]}
+        create_highlight(50, 740, 560, 800, color=(1, 1, 0)),
+        create_square(50, 650, 500, 680, color=(1, 0, 0)),
+        create_circle(50, 700, 550, 720, color=(0, 0, 0))]}
 
-highlight_file('data/mipt_conference.pdf', 'data/mipt_conference_out.pdf', highlights_dict)
+highlight_file('data/mipt_conference.pdf', 'data/mipt_conference_out.pdf', annotations_dict)
--- pdf_highlighter.py
+++ pdf_highlighter.py
@@ -3,44 +3,63 @@ from PyPDF2.generic import (
     DictionaryObject,
     FloatObject,
     NameObject,
-    ArrayObject,
+    ArrayObject
 )
 
 
 # x1, y1 starts in bottom left corner
-def highlight(x1, y1, x2, y2, color=(1, 0, 0)):
-    highlight = DictionaryObject()
+def _create_annotation(x1, y1, x2, y2, color, subtype):
+    annotation = DictionaryObject()
 
-    highlight.update({
-        NameObject('/Subtype'): NameObject('/Highlight'),
+    annotation.update({
+        NameObject('/Subtype'): NameObject(subtype),
         NameObject('/C'): ArrayObject([FloatObject(c) for c in color]),
         NameObject('/Rect'): ArrayObject([
             FloatObject(x1),
             FloatObject(y1),
             FloatObject(x2),
-            FloatObject(y2)
-        ]),
+            FloatObject(y2)]),
     })
 
-    return highlight
+    return annotation
 
 
-def _add_highlight(highlight, page):
+def _add_annotation(annotation, page):
     if '/Annots' in page:
-        page[NameObject('/Annots')].append(highlight)
+        page[NameObject('/Annots')].append(annotation)
     else:
-        page[NameObject('/Annots')] = ArrayObject([highlight])
+        page[NameObject('/Annots')] = ArrayObject([annotation])
 
 
-def highlight_file(file_in, file_out, highlights_dict):
+def create_highlight(x1, y1, x2, y2, color=(1, 0, 0)):
+    return _create_annotation(x1, y1, x2, y2, color, '/Highlight')
+
+
+def create_circle(x1, y1, x2, y2, color=(1, 0, 0)):
+    return _create_annotation(x1, y1, x2, y2, color, '/Circle')
+
+
+def create_square(x1, y1, x2, y2, color=(1, 0, 0)):
+    return _create_annotation(x1, y1, x2, y2, color, '/Square')
+
+
+def create_underline(x1, y1, x2, y2, color=(1, 0, 0)):
+    return _create_annotation(x1, y1, x2, y2, color, '/Underline')
+
+
+def create_strike_out(x1, y1, x2, y2, color=(1, 0, 0)):
+    return _create_annotation(x1, y1, x2, y2, color, '/StrikeOut')
+
+
+def highlight_file(file_in, file_out, annotations_dict):
     pdf_input = PdfFileReader(open(file_in, 'rb'))
     pdf_output = PdfFileWriter()
 
     for page_number in range(pdf_input.getNumPages()):
         page = pdf_input.getPage(page_number)
-        if page_number in highlights_dict:
-            for h in highlights_dict[page_number]:
-                _add_highlight(h, page)
+        if page_number in annotations_dict:
+            for annotation in annotations_dict[page_number]:
+                _add_annotation(annotation, page)
         pdf_output.addPage(page)
 
     output_stream = open(file_out, 'wb')
